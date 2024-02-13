f1f1bdcb8d0aa648c9896060b542322a9bb027de
milad-hub <milad445@gmail.com>
2024-02-13 16:20:15 +0330

    fixed some issues

--- android/src/main/java/com/osmdroid/TileCacherModule.java
+++ android/src/main/java/com/osmdroid/TileCacherModule.java
@@ -3,14 +3,15 @@ package com.osmdroid;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.osmdroid.OsmMapTileCacher;
 
 public class TileCacherModule extends ReactContextBaseJavaModule {
 
-    private final TileDatabaseHelper cacher;
+    private final OsmMapTileCacher cacher;
 
     public TileCacherModule(ReactApplicationContext reactContext) {
         super(reactContext);
-        this.cacher = new TileDatabaseHelper(reactContext);
+        this.cacher = new OsmMapTileCacher(reactContext);
     }
 
     @Override
